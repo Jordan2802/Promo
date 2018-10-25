13ba0ed0beb244077829d1df06f8847a7d85a41f
greglab74 <39262246+greglab74@users.noreply.github.com>
2018-10-25 15:19:56 +0200

    Update FormUser.php

--- projet/web/User/FormUser.php
+++ projet/web/User/FormUser.php
@@ -1,15 +1,57 @@
 
-
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Formulaire pour ajouter un utilisateur</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Simplon Charleville- Formulaire pour ajouter un utilisateur</title>
+</head>
+<!-- bootstrap Style CSS File -->
+<link rel="stylesheet" href="../css/bootstrap.min.css">
+<!-- Custom Style CSS File -->
+<link rel="stylesheet" type="text/css" href="../css/custom-style.css">
+<!-- Font-Awesome Style CSS File -->
+<link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.min.css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="../css/main.css" />
     
-</head>
+<body>
+
+<!-- Top navigation -->
+<nav class="navbar navbar-expand-md fixed-top top-nav">
+	<div class="container">
+		  <a class="navbar-brand" href="index.php"><strong>SIMPLON CHARLEVILLE</strong></a>
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"><i class="fa fa-bars" aria-hidden="true"></i></span>
+		  </button>
+
+		  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+		    <ul class="navbar-nav ml-auto">
+		      
+              
+		      
+		      <li class="nav-item">
+		        <a class="nav-link" href="../index.php">ACCUEIL</a>
+					</li>
+					<li class="nav-item">
+		        <a class="nav-link" href="connexion.php">Connexion</a>
+                <li class="nav-item active">
+		        <a class="nav-link" href="indexFormUser.php">Créer un compte</a>
+		      </li>
+		      </li>
+		    </ul>
+		  </div>	
+	</div>
+</nav>
+
+<!-- Intro Seven -->
+<section class="intro intro-small intro-bg bg-overlay-light parallax">
+	<div class="caption-container p-5">
+		<div class="intro-caption text-center mt-5">
+			<h1 class="display-4 text-white mb-2">1ere PROMO</h1>
+		</div>
+	</div>
+</section>
+
+
 <body>
 
 <h1>Inscription</h1>

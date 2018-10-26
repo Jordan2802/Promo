6354d29a4ec806dfa83a881c8b679ad45b32a0ef
Astyan8888 <liberden@gmail.com>
2018-10-26 13:55:44 +0200

    retrait partie footer

--- projet/src/App/Manager/UserManager.php
+++ projet/src/App/Manager/UserManager.php
@@ -11,7 +11,8 @@ use App\Entity\User;
 
 
 
-class UserManager{
+class UserManager
+{
 
 
     /**
@@ -35,21 +36,21 @@ class UserManager{
      * UserManager  constructor.
      * initialisation de la connexion à la base de donnée.
      */
-    public function __construct(){
+    public function __construct()
+    {
+        $host_name = 'db759087613.hosting-data.io';
+        $database = 'db759087613';
+        $user_name = 'dbo759087613';
+        $password = 'azerty5488';
 
-       // $host_name = 'db759087613.hosting-data.io';
-       // $database = 'db759087613';
-       // $user_name = 'dbo759087613';
-       // $password = 'azerty5488';
+        $this->pdo = new PDO("mysql:host=$host_name; dbname=$database;", $user_name, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
 
-       // $this->pdo = new PDO("mysql:host=$host_name; dbname=$database;", $user_name, $password,array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8') );
-        $host_name = 'localhost';
-        $database = 'promo';
-        $user_name = 'root';
-        $password = '';
+        // $host_name = 'localhost';
+        // $database = 'promo';
+        // $user_name = 'root';
+        // $password = '';
 
-        $this->pdo = new PDO("mysql:host=$host_name; dbname=$database;", $user_name, $password,array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8') );
-
+        // $this->pdo = new PDO("mysql:host=$host_name; dbname=$database;", $user_name, $password,array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8') );
     }
 
     /**
@@ -59,8 +60,8 @@ class UserManager{
      * @param User $user    objet de type User passé par référence.
      * @return bool true si l'objet a été inseré, false si une erreur survient
      */
-    private function create(User &$user){
-
+    private function create(User &$user)
+    {
         $this->pdoStatement = $this->pdo->prepare('INSERT INTO userpromo VALUES (NULL, :name, :firstname, :mail, :password, :age, :citation, :language, :project, :photo ) ');
 
         //liaison des parametres
@@ -80,16 +81,13 @@ class UserManager{
 
         $executeIsOk = $this->pdoStatement->execute();
 
-        if(!$executeIsOk){
+        if (!$executeIsOk) {
             return false;
-        }
-        else{
-
+        } else {
             $id = $this->pdo->lastInsertId();
             $user = $this->read($id);
             return true;
         }
-
     }
 
 
@@ -99,35 +97,30 @@ class UserManager{
      * @param int  $id  identifiant d'un contact
      * @return bool|User|null false si une erreur survient, un objet user si une sorrespondance est trouvée, Null
      */
-    public function read($id){
+    public function read($id)
+    {
+        $this->pdoStatement =  $this->pdo->prepare('SELECT * FROM userpromo WHERE ID_user= :id ');
 
-      $this->pdoStatement =  $this->pdo->prepare('SELECT * FROM userpromo WHERE ID_user= :id ');
+        //liaison des parametres
 
-      //liaison des parametres
+        $this->pdoStatement->bindValue(':id', $id, PDO::PARAM_INT);
 
-      $this->pdoStatement->bindValue(':id',$id, PDO::PARAM_INT);
+        //éxécution de la requête
 
-      //éxécution de la requête
+        $executeIsOk = $this->pdoStatement->execute();
 
-      $executeIsOk = $this->pdoStatement->execute();
+        if ($executeIsOk) {
+            $user = $this->pdoStatement-> fetchObject('App\Entity\User');
 
-      if($executeIsOk){
-
-        $user = $this->pdoStatement-> fetchObject('App\Entity\User');
-
-        if($user===false){
-            return null;
+            if ($user===false) {
+                return null;
+            } else {
+                return $user;
+            }
+        } else {
+            //erreur d'execution
+            return false;
         }
-        else{
-            return $user;
-        }
-
-      }
-      else{
-          //erreur d'execution
-          return false;
-      }
-
     }
 
 
@@ -137,9 +130,8 @@ class UserManager{
      * @return array|bool tableau d'objet User ou un tableau vide s'il n'y a aucun objet dans la
      * bdd, ou false si une erreur survient
      */
-    public function readAll(){
-
-
+    public function readAll()
+    {
         $this->pdoStatement = $this->pdo->query('SELECT * FROM userpromo ORDER BY name, mail');
 
         //construction d'un tableau d'objet de type User
@@ -154,8 +146,8 @@ class UserManager{
      * @param User $user objet de type User
      * @return bool true en cas de succès ou false en cas d'erreur
      */
-    private function update(User $user){
-
+    private function update(User $user)
+    {
         $pdoStatement = $this->pdo->prepare('UPDATE userpromo SET name=:name, firstname = :firstname Password=:Password, mail=:Mail, age=:age, citation=:citation, language=:language, project=:project, photo=:photo WHERE ID_user=:id LIMIT 1');
 
         //liaison des parametres
@@ -174,7 +166,6 @@ class UserManager{
         //execution de la requete
 
         return $pdoStatement->execute();
-
     }
 
     /**
@@ -183,8 +174,8 @@ class UserManager{
      * @param User $user objet de type User
      * @return bool true en cas de succès ou false en cas d'erreur
      */
-    public function delete(User $user){
-
+    public function delete(User $user)
+    {
         $pdoStatement = $this->pdo->prepare('DELETE  FROM userpromo WHERE ID_user = :id LIMIT 1');
 
         $pdoStatement->bindValue(':id', $user->getId(), PDO::PARAM_INT);
@@ -192,7 +183,6 @@ class UserManager{
         //execution de la requete
 
         return $pdoStatement->execute();
-
     }
 
 
@@ -203,35 +193,32 @@ class UserManager{
      * @param User $user    objet User passé par référence (&)
      * @return bool true en cas de succes ou false en cas d'erreur
      */
-    public function save(User &$user){
-
-        if(is_null($user->getId())){
+    public function save(User &$user)
+    {
+        if (is_null($user->getId())) {
             return $this->create($user);
-        }
-        else{
+        } else {
             return $this->update($user);
         }
-
     }
 
 
-/**
- * methode pour verifier si l'email existe dans la base de donnée
- *
- * @param string $mail
- * @return bool
- */
-    public function verifMail($mail){
-
+    /**
+     * methode pour verifier si l'email existe dans la base de donnée
+     *
+     * @param string $mail
+     * @return bool
+     */
+    public function verifMail($mail)
+    {
         $pdoStatement = $this->pdo->prepare('SELECT COUNT(*) FROM userpromo WHERE mail = :Mail');
         $pdoStatement->execute(array(':Mail'=>$mail));
         $mailExist = $pdoStatement->fetch()[0];
         if ($mailExist == 0) {
             return true;
         } else {
-           return false;
+            return false;
         }
-
     }
 
 
@@ -242,35 +229,23 @@ class UserManager{
      *
      * @return void
      */
-    public function login($mail){
-
-        if(!empty($mail)){
+    public function login($mail)
+    {
+        if (!empty($mail)) {
             $pdoStatement = $this->pdo->prepare('SELECT * FROM userpromo WHERE mail = :mail ');
-            $pdoStatement->execute(array(':mail'=>$mail
+            $pdoStatement->execute(
+                array(':mail'=>$mail
                 )
             );
             $count = $pdoStatement->rowCount();
-            if($count>0){
-
+            if ($count>0) {
                 $_SESSION['mail'] = $mail;
 
                 return $pdoStatement->fetch();
-            }else{
+            } else {
                 $message="false";
                 echo $message;
             }
         }
     }
-
-
-
-
-
-
-
-
-
-
-
-
 }
